7258f18f169b061a994a0f1f40be41d6536384e6
Ryyyyyy12 <kevalin.sris@kmutt.ac.th>
2022-05-29 15:08:50 +0700

    W3-04 Intro. to Redux

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,12 +5,23 @@ import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
+import { connect } from 'react-redux';
+
 import Contact from './ContactComponent';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
 
+const mapStateToProps = state => {
+    return {
+      dishes: state.dishes,
+      comments: state.comments,
+      promotions: state.promotions,
+      leaders: state.leaders
+    }
+  }
+  
 class Main extends Component {
 
     constructor(props) {
@@ -33,27 +44,31 @@ class Main extends Component {
     const HomePage = () => {
         return(
             <Home 
-                dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-                promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
-                leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+                promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+                leader={this.props.leaders.filter((leader) => leader.featured)[0]}
             />
         );
-      }
+    }
+  
   
   
     return (
-      <div>
-        <Header />
-        <Switch>
-              <Route path='/home' component={HomePage} />
-              <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} selectedDish={this.state.selectedDish} onClick={(dishId)=>this.onDishSelect(dishId)} comments={this.state.comments}/>} />
-              <Route exact path='/contactus' component={Contact} />
-              <Redirect to="/home" />
-        </Switch>
-        <Footer />
-      </div>
-    );
+        <div>
+          <Header />
+          <div>
+            <Switch>
+                <Route path='/home' component={HomePage} />
+                <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
+                <Route exact path='/contactus' component={Contact} />} />
+                <Redirect to="/home" />
+            </Switch>
+          </div>
+          <Footer />
+        </div>
+      );
+  
   }
 }
 
-export default Main;
+export default withRouter(connect(mapStateToProps)(Main));
